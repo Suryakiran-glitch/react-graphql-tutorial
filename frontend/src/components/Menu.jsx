48c7906dc3f5835f23a413fb51318ede3209c608
Surya Kiran <settisurya5@gmail.com>
2021-01-28 20:55:14 +0530

    Added logout functionality

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -1,13 +1,24 @@
 import React, { useState } from 'react'
 import { Menu } from 'semantic-ui-react'
 import {Link} from 'react-router-dom'
+import {useHistory} from 'react-router-dom'
+import cookie from 'js-cookie'
 
 const MenuExampleBasic  = () => {
+
+  const history = useHistory()
 
  const pathname = window.location.pathname
  const path = pathname === '/' ? 'home' : pathname.substr(1)
 
  const [activeItem , setactiveItem] = useState(path)
+
+ const logoutHandler = () => {
+   cookie.remove('token')
+   cookie.remove('username')
+   cookie.remove('id')
+   history.push('/login')
+ }
   
 
   const handleItemClick = (e, { name }) => setactiveItem(name)
@@ -37,6 +48,12 @@ const MenuExampleBasic  = () => {
           onClick={handleItemClick}
           as={Link}
           to="/login"
+        />
+
+<Menu.Item        
+          name='login'
+          active={activeItem === 'login'}
+          onClick={logoutHandler}
         />
           
       </Menu>
